e201917081bc91689526e52cf05acad0711f536f
ranjahasrama <ranjahasrama@gmail.com>
2024-11-22 12:40:51 +0100

    Created offers-list component

---
+++ parking-app/src/app/features/offers/components/offers-list/offers-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './offers-list.component.html',
+  styleUrl: './offers-list.component.css'
+})
+export class OffersListComponent {
+
+}
