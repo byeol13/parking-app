5a0126646e8d469aebd86be4229bd466a5aad57d
ranjahasrama <ranjahasrama@gmail.com>
2024-11-22 13:04:36 +0100

    Shfaq Menu ne Dashboard Component

--- parking-app/src/app/app.routes.ts
+++ parking-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './features/customer/components/customer-list/customer-list.component';
+import { OffersListComponent } from './features/offers/components/offers-list/offers-list.component';
+import { ParkingLotListComponent } from './features/parkingLot/components/parking-lot-list/parking-lot-list.component';
+import { ParkingMonthlyPassListComponent } from './features/parkingMonthlyPass/components/parking-monthly-pass-list/parking-monthly-pass-list.component';
+import { ParkingOneTimeResListComponent } from './features/parkingOneTimeReservation/components/parking-one-time-res-list/parking-one-time-res-list.component';
+import { ParkingPricingListComponent } from './features/parkingPricing/components/parking-pricing-list/parking-pricing-list.component';
+import { PaymentMethodListComponent } from './features/paymentMethod/components/payment-method-list/payment-method-list.component';
+import { PricingExceptionListComponent } from './features/pricingException/components/pricing-exception-list/pricing-exception-list.component';
+import { VehicleListComponent } from './features/vehicle/components/vehicle-list/vehicle-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard/customer', component: CustomerListComponent},
+  {path: 'dashboard/offers', component: OffersListComponent},
+  {path: 'dashboard/parkingLot', component: ParkingLotListComponent},
+  {path: 'dashboard/parkingMonthlyPass', component: ParkingMonthlyPassListComponent},
+  {path: 'dashboard/parkingOneTimeReservation', component: ParkingOneTimeResListComponent},
+  {path: 'dashboard/parkingPricing', component: ParkingPricingListComponent},
+  {path: 'dashboard/paymentMethod', component: PaymentMethodListComponent},
+  {path: 'dashboard/pricingException', component: PricingExceptionListComponent},
+  {path: 'dashboard/vehicle', component: VehicleListComponent}
+];
--- parking-app/src/app/dashboard/dashboard.component.ts
+++ parking-app/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  routes: {path: string, label: string}[] = [
+    {path: '/dashboard/customer', label: 'Customer'},
+    {path: '/dashboard/offers', label: 'Offers'},
+    {path: '/dashboard/parkingLot', label: 'Parking Lot'},
+    {path: '/dashboard/parkingMonthlyPass', label: 'Parking Monthly Pass'},
+    {path: '/dashboard/parkingOneTimeReservation', label: 'One Time Reservation'},
+    {path: '/dashboard/parkingPricing', label: 'Parking Pricing'},
+    {path: '/dashboard/paymentMethod', label: 'Payment Method'},
+    {path: '/dashboard/pricingException', label: 'Pricing Exceptions'},
+    {path: '/dashboard/vehicle', label: 'Vehicles'}
+  ]
 
 }
