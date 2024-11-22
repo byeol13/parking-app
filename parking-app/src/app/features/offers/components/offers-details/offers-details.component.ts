9b14fded41af12a78f54bee0ac41283980f7dfb6
ranjahasrama <ranjahasrama@gmail.com>
2024-11-22 12:41:41 +0100

    Created offers-details component

---
+++ parking-app/src/app/features/offers/components/offers-details/offers-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './offers-details.component.html',
+  styleUrl: './offers-details.component.css'
+})
+export class OffersDetailsComponent {
+
+}
