abc32177a8f558aa89099586706a2d6d470b3291
ranjahasrama <ranjahasrama@gmail.com>
2024-11-22 12:23:12 +0100

    Created paymentMethod-details component

---
+++ parking-app/src/app/features/paymentMethod/components/payment-method-details/payment-method-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-method-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-method-details.component.html',
+  styleUrl: './payment-method-details.component.css'
+})
+export class PaymentMethodDetailsComponent {
+
+}
