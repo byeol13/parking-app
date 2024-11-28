2c45f52d8997478f3e9ddf9913914e34a97769c8
ranjahasrama <ranjahasrama@gmail.com>
2024-11-28 16:32:32 +0100

    Shfaq te gjitha te dhenat qe ka tabela parkingMonthlyPass ne Parking Monthly Pass menu/ ne dashboard

--- parking-app/src/app/features/parkingMonthlyPass/components/parking-monthly-pass-list/parking-monthly-pass-list.component.ts
+++ parking-app/src/app/features/parkingMonthlyPass/components/parking-monthly-pass-list/parking-monthly-pass-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ParkingMonthlyPassService } from '../../service/parking-monthly-pass.service';
+import { ParkingMonthlyPass } from '../../../../shared/models/ParkingMonthlyPass.model';
 
 @Component({
   selector: 'app-parking-monthly-pass-list',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatTableModule],
   templateUrl: './parking-monthly-pass-list.component.html',
   styleUrl: './parking-monthly-pass-list.component.css'
 })
-export class ParkingMonthlyPassListComponent {
+export class ParkingMonthlyPassListComponent implements OnInit{
+
+  monthlyPasses: ParkingMonthlyPass[] = [];
+  displayedColumns: string[] = ['id', 'customerName', 'parkingLotAddress', 'startDate', 'duration', 'cost', 'reentryAllowed'];
+
+  constructor(private parkingMonthlyPassService: ParkingMonthlyPassService){}
+
+  ngOnInit(): void {
+    this.loadAllMonthlyPasses();
+  }
+
+  loadAllMonthlyPasses() {
+    this.parkingMonthlyPassService.getAllParkingMonthlyPasses().subscribe((res) => {
+      this.monthlyPasses = res;
+    })
+  }
 
 }
---
+++ parking-app/src/app/features/parkingMonthlyPass/service/parking-monthly-pass.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ParkingMonthlyPass } from '../../../shared/models/ParkingMonthlyPass.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParkingMonthlyPassService {
+
+  private apiUrl = `http://localhost:3000/parking_monthly_pass`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllParkingMonthlyPasses(): Observable<ParkingMonthlyPass[]> {
+    return this.http.get<ParkingMonthlyPass[]>(this.apiUrl);
+  }
+}
