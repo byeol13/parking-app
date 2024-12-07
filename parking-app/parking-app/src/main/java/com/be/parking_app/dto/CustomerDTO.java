9fc2e88aab4813a64c1c5675c9f9e7e4a3025dc1
ranjahasrama <ranjahasrama@gmail.com>
2024-12-07 14:57:05 +0100

    8696zvyga- Create DTO class for customer table

---
+++ parking-app/parking-app/src/main/java/com/be/parking_app/dto/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.be.parking_app.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+public class CustomerDTO {
+    private Integer id;
+    private LocalDate registrationDate;
+    private String isRegularCusto;
+    private String contactNumber;
+    private String firstName;
+    private String lastName;
+    private String billingAddress;
+}
