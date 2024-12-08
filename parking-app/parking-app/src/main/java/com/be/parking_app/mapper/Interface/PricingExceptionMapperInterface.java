854de543eadd582351f2945af912d23b0d9cec85
ranjahasrama <ranjahasrama@gmail.com>
2024-12-08 17:01:48 +0100

    8697021rg- Create mapper interface for Pricing Exception table

---
+++ parking-app/parking-app/src/main/java/com/be/parking_app/mapper/Interface/PricingExceptionMapperInterface.java
@@ -0,0 +1,9 @@
+package com.be.parking_app.mapper.Interface;
+
+import com.be.parking_app.dto.PricingExceptionDTO;
+import com.be.parking_app.entity.PricingExceptionEntity;
+
+public interface PricingExceptionMapperInterface {
+    PricingExceptionDTO toDTO(PricingExceptionEntity entity);
+    PricingExceptionEntity toEntity(PricingExceptionDTO dto);g
+}
