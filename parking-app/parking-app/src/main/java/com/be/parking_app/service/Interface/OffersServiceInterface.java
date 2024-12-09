288377b6eb0328007ebbba935d5fc87503e66e48
ranjahasrama <ranjahasrama@gmail.com>
2024-12-09 17:34:40 +0100

    86970g3cc- Create service interface for Offers table

---
+++ parking-app/parking-app/src/main/java/com/be/parking_app/service/Interface/OffersServiceInterface.java
@@ -0,0 +1,18 @@
+package com.be.parking_app.service.Interface;
+
+import com.be.parking_app.dto.OffersDTO;
+
+import java.util.List;
+
+public interface OffersServiceInterface {
+
+    List<OffersDTO> getAllOfferData();
+
+    OffersDTO getOfferObjectById(Integer id);
+
+    OffersDTO insertNewOfferObjectData(OffersDTO body);
+
+    OffersDTO updateExistingOfferObjectData(OffersDTO body);
+
+    OffersDTO deleteOfferObjectData(Integer id);
+}
