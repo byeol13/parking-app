2faa780e4644d51e07d5a7fb7c9fea633b305dd4
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 18:01:01 +0100

    8697145em- Create API to get all offers

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/OffersController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/OffersController.java
@@ -1,13 +1,27 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.OffersDTO;
+import com.be.parking_app.service.Impl.OffersServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/offers")
 public class OffersController {
+
+    @Autowired
+    private OffersServiceImpl service;
+
+    @GetMapping("/getAllOffers")
+    private List<OffersDTO> getAllOffers() {
+        return service.getAllOffersData();
+    }
 }
