74712420771bc5317201246d57ae468659af3610
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 16:36:57 +0100

    869710y73- Create API to get all parking lots

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingLotController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingLotController.java
@@ -1,13 +1,27 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.ParkingLotDTO;
+import com.be.parking_app.service.Impl.ParkingLotServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/parking-lot")
 public class ParkingLotController {
+
+    @Autowired
+    private ParkingLotServiceImpl service;
+
+    @GetMapping("/getAllParkingLots")
+    public List<ParkingLotDTO> getAllParkingLot() {
+        return service.getAllParkingLotData();
+    }
 }
