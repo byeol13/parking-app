4db46caf10c9ddee84cc51cf56b7f65652cd1745
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 16:04:35 +0100

    869710df1- Create API to get all customers

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/CustomerController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/CustomerController.java
@@ -1,13 +1,27 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.CustomerDTO;
+import com.be.parking_app.service.Impl.CustomerServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
+
+    @Autowired
+    private CustomerServiceImpl service;
+
+    @GetMapping("/getAllCustomers")
+    public List<CustomerDTO> getAllCustomers() {
+        return service.getAllCustomerData();
+    }
 }
