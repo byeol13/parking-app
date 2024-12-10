0613e0e9065d7ba3518c79c9eee8b96003fe8218
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 17:23:00 +0100

    869710zqf- Create API to get all vehicles

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/VehicleController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/VehicleController.java
@@ -1,13 +1,27 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.VehicleServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
+
+    @Autowired
+    private VehicleServiceImpl service;
+
+    @GetMapping("/getAllVehicles")
+    private List<VehicleDTO> getAllVehicle() {
+        return service.getAllVehicleData();
+    }
 }
