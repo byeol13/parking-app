977510b3554938c0a6635b838157fd44ecd89241
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 17:26:27 +0100

    86971109n- Create API to get vehicle by id

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/VehicleController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/VehicleController.java
@@ -4,10 +4,8 @@ import com.be.parking_app.dto.VehicleDTO;
 import com.be.parking_app.service.Impl.VehicleServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class VehicleController {
     @GetMapping("/getAllVehicles")
     private List<VehicleDTO> getAllVehicle() {
         return service.getAllVehicleData();
+    }
+
+    @GetMapping("/getVehicleById")
+    public VehicleDTO getVehicleById(@Validated @RequestParam(name = "vehicleId") Integer id) {
+        return service.getVehicleObjectById(id);
     }
 }
