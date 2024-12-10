18077a6c6e197945336fccc4649fce01375b1a2b
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 16:40:23 +0100

    869710ycq- Create API to get parking lot by id

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingLotController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingLotController.java
@@ -4,10 +4,8 @@ import com.be.parking_app.dto.ParkingLotDTO;
 import com.be.parking_app.service.Impl.ParkingLotServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class ParkingLotController {
     @GetMapping("/getAllParkingLots")
     public List<ParkingLotDTO> getAllParkingLot() {
         return service.getAllParkingLotData();
+    }
+
+    @GetMapping("/getParkingLotById")
+    public ParkingLotDTO getParkingLotById(@Validated @RequestParam(name = "parkingLotId") Integer id) {
+        return service.getParkingLotObjectById(id);
     }
 }
