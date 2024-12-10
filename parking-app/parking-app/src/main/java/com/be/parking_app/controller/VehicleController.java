ccebec8616e2760b35be6b38c193028dca79e741
ranjahasrama <ranjahasrama@gmail.com>
2024-12-10 17:13:09 +0100

    869710zg7- Create controller class for Vehicle table

---
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/VehicleController.java
@@ -0,0 +1,13 @@
+package com.be.parking_app.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+}
