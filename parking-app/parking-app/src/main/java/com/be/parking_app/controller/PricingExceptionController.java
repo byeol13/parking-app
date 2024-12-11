796b6496bbdf77d441af12e79df1b0b67b975d56
ranjahasrama <ranjahasrama@gmail.com>
2024-12-11 15:31:01 +0100

    86971j25c- Create API to get all pricing exceptions

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/PricingExceptionController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/PricingExceptionController.java
@@ -1,13 +1,28 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.PricingExceptionDTO;
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.PricingExceptionServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/pricingException")
 public class PricingExceptionController {
+
+    @Autowired
+    private PricingExceptionServiceImpl service;
+
+    @GetMapping("/getAllPricingExceptions")
+    private List<PricingExceptionDTO> getAllPricingException() {
+        return service.getAllPricingExceptionData();
+    }
 }
