5079ca2c29d89b7eabc0a2f6c5143fe5110491ce
ranjahasrama <ranjahasrama@gmail.com>
2024-12-11 16:43:06 +0100

    86971jpqm- Create API to get all parking one time reservations

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingOneTimeReservationController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingOneTimeReservationController.java
@@ -1,13 +1,28 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.ParkingOneTimeReservationDTO;
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.ParkingOneTimeReservationServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/vehicles")
 public class ParkingOneTimeReservationController {
+
+    @Autowired
+    private ParkingOneTimeReservationServiceImpl service;
+
+    @GetMapping("/getAllOneTimeReservations")
+    private List<ParkingOneTimeReservationDTO> getAllParkingOneTimeReservations() {
+        return service.getAllParkingOneTimeReservationData();
+    }
 }
