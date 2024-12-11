6313457f42452333b49df34d9b6d1d1fdcb97473
ranjahasrama <ranjahasrama@gmail.com>
2024-12-11 15:46:29 +0100

    86971jh81- Create API to get all payment methods

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/PaymentMethodController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/PaymentMethodController.java
@@ -1,13 +1,28 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.PaymentMethodDTO;
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.PaymentMethodServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/paymentMethod")
 public class PaymentMethodController {
+
+    @Autowired
+    private PaymentMethodServiceImpl service;
+
+    @GetMapping("/getALlPaymentMethods")
+    private List<PaymentMethodDTO> getAllPaymentMethods() {
+        return service.getAllPaymentMethodData();
+    }
 }
