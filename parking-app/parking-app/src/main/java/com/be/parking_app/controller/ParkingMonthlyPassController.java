8468df526057b880803675a79b04962265558675
ranjahasrama <ranjahasrama@gmail.com>
2024-12-11 16:08:00 +0100

    86971jmjq- Create API to get all parking monthly passes

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingMonthlyPassController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingMonthlyPassController.java
@@ -1,13 +1,28 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.ParkingMonthlyPassDTO;
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.ParkingMonthlyPassServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/parkingMonthlyPass")
 public class ParkingMonthlyPassController {
+
+    @Autowired
+    private ParkingMonthlyPassServiceImpl service;
+
+    @GetMapping("/getAllMonthlyPasses")
+    private List<ParkingMonthlyPassDTO> getAllMonthlyPasses() {
+        return service.getAllParkingMonthlyPassData();
+    }
 }
