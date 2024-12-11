87888ff40214630bdb64eaf8176bd271d33c3cc0
ranjahasrama <ranjahasrama@gmail.com>
2024-12-11 15:17:47 +0100

    86971j0c3- Create API to get all parking pricing

--- parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingPricingController.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/controller/ParkingPricingController.java
@@ -1,13 +1,28 @@
 package com.be.parking_app.controller;
 
+import com.be.parking_app.dto.ParkingPricingDTO;
+import com.be.parking_app.dto.VehicleDTO;
+import com.be.parking_app.service.Impl.ParkingPricingServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/parkingPricing")
 public class ParkingPricingController {
+
+    @Autowired
+    private ParkingPricingServiceImpl service;
+
+    @GetMapping("/getAllParkingPricing")
+    private List<ParkingPricingDTO> getAllParkingPricing() {
+        return service.getAllParkingPricingData();
+    }
 }
