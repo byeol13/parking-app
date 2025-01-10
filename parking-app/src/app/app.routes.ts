f19d1b7f1957b21c386656dd4a0df93d0c582cff
ranjahasrama <ranjahasrama@gmail.com>
2025-01-10 18:58:44 +0100

    Modified parking pricing list component

--- parking-app/src/app/app.routes.ts
+++ parking-app/src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { ParkingLotListComponent } from './features/parkingLot/components/parkin
 import { ParkingMonthlyPassListComponent } from './features/parkingMonthlyPass/components/parking-monthly-pass-list/parking-monthly-pass-list.component';
 import { ParkingOneTimeResListComponent } from './features/parkingOneTimeReservation/components/parking-one-time-res-list/parking-one-time-res-list.component';
 import { ParkingPricingListComponent } from './features/parkingPricing/components/parking-pricing-list/parking-pricing-list.component';
-import { PaymentMethodListComponent } from './features/paymentMethod/components/payment-method-list/payment-method-list.component';
 import { PricingExceptionListComponent } from './features/pricingException/components/pricing-exception-list/pricing-exception-list.component';
 import { VehicleListComponent } from './features/vehicle/components/vehicle-list/vehicle-list.component';
 import { CustomerDetailsComponent } from './features/customer/components/customer-details/customer-details.component';
@@ -14,13 +13,11 @@ import { ParkingLotDetailsComponent } from './features/parkingLot/components/par
 import { OffersDetailsComponent } from './features/offers/components/offers-details/offers-details.component';
 import { VehicleDetailsComponent } from './features/vehicle/components/vehicle-details/vehicle-details.component';
 import { PaymentMethodDetailsComponent } from './features/paymentMethod/components/payment-method-details/payment-method-details.component';
-import { ParkingPricingDetailsComponent } from './features/parkingPricing/components/parking-pricing-details/parking-pricing-details.component';
 import { PricingExceptionDetailsComponent } from './features/pricingException/components/pricing-exception-details/pricing-exception-details.component';
 import { ParkingOneTimeResDetailsComponent } from './features/parkingOneTimeReservation/components/parking-one-time-res-details/parking-one-time-res-details.component';
 import { ParkingMonthlyPassDetailsComponent } from './features/parkingMonthlyPass/components/parking-monthly-pass-details/parking-monthly-pass-details.component';
 import { ReservationsMenuComponent } from './features/reservations/components/reservations-menu/reservations-menu.component';
 import { AddParkingLotComponent } from './features/parkingLot/components/add-parking-lot/add-parking-lot.component';
-import { CurrentParkingPricingComponent } from './features/parkingPricing/components/current-parking-pricing/current-parking-pricing.component';
 import { AddParkingPricingComponent } from './features/parkingPricing/components/add-parking-pricing/add-parking-pricing.component';
 
 export const routes: Routes = [
@@ -38,9 +35,8 @@ export const routes: Routes = [
   {path: 'parkingOneTimeReservation', component: ParkingOneTimeResDetailsComponent},
   {path: 'dashboard/parkingPricing', component: ParkingPricingListComponent},
   // {path: 'parkingPricing', component: ParkingPricingDetailsComponent},
-  {path: 'currentParkingPricing/:parkingLotId', component: CurrentParkingPricingComponent},
+  {path: 'currentParkingPricing/:parkingLotId', component: ParkingPricingListComponent},
   {path: 'addParkingPricing', component: AddParkingPricingComponent},
-  {path: 'dashboard/paymentMethod', component: PaymentMethodListComponent},
   {path: 'paymentMethod', component: PaymentMethodDetailsComponent},
   {path: 'dashboard/pricingException', component: PricingExceptionListComponent},
   {path: 'pricingException', component: PricingExceptionDetailsComponent},
--- parking-app/src/app/features/parkingPricing/components/parking-pricing-list/parking-pricing-list.component.ts
+++ parking-app/src/app/features/parkingPricing/components/parking-pricing-list/parking-pricing-list.component.ts
@@ -3,44 +3,139 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ParkingPricingService } from '../../service/parking-pricing.service';
 import { ParkingPricing } from '../../../../shared/models/ParkingPricing.model';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { ParkingPricingDeleteComponent } from '../parking-pricing-delete/parking-pricing-delete.component';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { AddParkingPricingDialogComponent } from '../add-parking-pricing-dialog/add-parking-pricing-dialog.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-parking-pricing-list',
   standalone: true,
-  imports: [MatToolbarModule, MatTableModule, MatButtonModule, CommonModule],
+  imports: [MatTableModule, MatCardModule, CommonModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatOptionModule, MatSelectModule, ReactiveFormsModule, MatInputModule, ParkingPricingDeleteComponent],
   templateUrl: './parking-pricing-list.component.html',
   styleUrl: './parking-pricing-list.component.css'
 })
 export class ParkingPricingListComponent implements OnInit{
 
-  parkingPricingIdToDelete: number | undefined;
+  // parkingPricingIdToDelete: number | undefined;
+  // parkingPricing: ParkingPricing[] = [];
+  // displayedColumns: string[] = ['id', 'parking_lot_address', 'day_of_week', 'morning_hr_cost', 'midday_hr_cost', 'evening_hr_cost', 'all_day_cost', 'actions'];
+  // showDeleteDialog = false;
+
+  // constructor(private parkingPricingService: ParkingPricingService, private router: Router){}
+
+  // ngOnInit(): void {
+  //   this.loadAllParkingPricings();  
+  // }
+
+  // loadAllParkingPricings() {
+  //   this.parkingPricingService.getAllParkingPricings().subscribe((res) => {
+  //     this.parkingPricing = res;
+  //   })
+  // }
+
+  // getDayOfWeek(day: number): string {
+  //   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  //   return daysOfWeek[day - 1];
+  // }
+
+  // viewDetails(id: number) {
+  //   this.router.navigate([`/parkingPricing`], {queryParams: { parkingPricingId: id }});
+  // }
+
+  // openDeleteDialog(id: number) {
+  //   this.parkingPricingIdToDelete = id;
+  //   this.showDeleteDialog = true;
+  // }
+
+  // deleteParkingPricingById(id: number) {
+  //   this.parkingPricingService.deleteParkingPricingById(id).subscribe(() => {
+  //     this.loadAllParkingPricings();
+  //     this.showDeleteDialog = false;
+  //   })
+  // }
+  
+  // cancelDelete() {
+  //   this.showDeleteDialog = false;
+  // }
+
   parkingPricing: ParkingPricing[] = [];
-  displayedColumns: string[] = ['id', 'parking_lot_address', 'day_of_week', 'morning_hr_cost', 'midday_hr_cost', 'evening_hr_cost', 'all_day_cost', 'actions'];
+  parkingLotId: any;
+  displayedColumns: string[] = ['dayOfWeek', 'morningHrCost', 'middayHrCost', 'eveningHrCost', 'allDayCost', 'actions'];
+  noPricingMessage: string = "No parking pricings available for this parking lot";
+
+  dayOfWeekNames: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  isAddingPricing = false;
   showDeleteDialog = false;
+  parkingPricingIdToDelete: number | undefined;
 
-  constructor(private parkingPricingService: ParkingPricingService, private router: Router){}
+  addPricingForm: FormGroup;
 
-  ngOnInit(): void {
-    this.loadAllParkingPricings();  
-  }
+  constructor(private parkingPricingService: ParkingPricingService, private activatedRoute: ActivatedRoute, private fb: FormBuilder, private dialog: MatDialog){
 
-  loadAllParkingPricings() {
-    this.parkingPricingService.getAllParkingPricings().subscribe((res) => {
-      this.parkingPricing = res;
+    this.addPricingForm = fb.group({
+      dayOfWeek: ['', Validators.required],
+      morningHrCost: ['', Validators.required],
+      middayHrCost: ['', Validators.required],
+      eveningHrCost: ['', Validators.required],
+      allDayCost: ['', Validators.required]
     })
   }
 
-  getDayOfWeek(day: number): string {
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    return daysOfWeek[day - 1];
+  ngOnInit(): void {
+    this.parkingLotId = +this.activatedRoute.snapshot.paramMap.get('parkingLotId')!;
+    this.loadParkingPricing();
   }
 
-  viewDetails(id: number) {
-    this.router.navigate([`/parkingPricing`], {queryParams: { parkingPricingId: id }});
+  loadParkingPricing() {
+    this.parkingPricingService.getAllParkingPricings().subscribe((res: ParkingPricing[]) => {
+      this.parkingPricing = res.filter(pricing => pricing.parkingLotDTO.parkingLotId === this.parkingLotId);
+    });
+  }
+
+  getDayName(day: number): string {
+    return this.dayOfWeekNames[day];
+  }
+
+  toggleAddPricing() {
+    this.isAddingPricing = !this.isAddingPricing;
+  }
+
+  onSubmit() {
+    if (this.addPricingForm.valid) {
+      const newParkingPricing = {
+        ...this.addPricingForm.value,
+        parkingLotDTO: {
+          parkingLotId: this.parkingLotId
+        }
+      };
+
+      this.parkingPricingService.addParkingPricing(newParkingPricing).subscribe(() => {
+
+        const successfulMessage = this.dialog.open(AddParkingPricingDialogComponent, {
+          width: '400px', height: '200px'
+        });
+
+        successfulMessage.afterClosed().subscribe(() => {
+          this.isAddingPricing = false;
+          this.addPricingForm.reset();
+          this.loadParkingPricing();
+        })
+      })
+    }
+  }
+
+  onCancel() {
+    this.isAddingPricing = false;  
+    this.addPricingForm.reset(); 
   }
 
   openDeleteDialog(id: number) {
@@ -50,13 +145,12 @@ export class ParkingPricingListComponent implements OnInit{
 
   deleteParkingPricingById(id: number) {
     this.parkingPricingService.deleteParkingPricingById(id).subscribe(() => {
-      this.loadAllParkingPricings();
+      this.loadParkingPricing();
       this.showDeleteDialog = false;
-    })
-  }
-  
-  cancelDelete() {
-    this.showDeleteDialog = false;
+    });
   }
 
+  onCancelDelete() {
+    this.showDeleteDialog = false;
+  }
 }
