b4d10fe9215f98c1fe5a7c2af3493d21aeb55639
ranjahasrama <ranjahasrama@gmail.com>
2025-01-23 12:21:44 +0100

    Modified Pricing repo file

--- parking-app/parking-app/src/main/java/com/be/parking_app/repository/ParkingPricingRepository.java
+++ parking-app/parking-app/src/main/java/com/be/parking_app/repository/ParkingPricingRepository.java
@@ -1,8 +1,11 @@
 package com.be.parking_app.repository;
 
+import com.be.parking_app.dto.ParkingPricingDTO;
 import com.be.parking_app.entity.ParkingPricingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ParkingPricingRepository extends JpaRepository<ParkingPricingEntity, Integer> {
